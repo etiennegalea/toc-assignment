8486c68c2cec132ec704f706f7f85ff5446a4e6c
Etienne Galea <egalea11@gmail.com>
2025-01-02 14:56:15 +0100

    implemented simple navbar and placeholder for saved article list

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,30 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
-  title: "ToC Assignemnt",
+  title: "ToC Assignment",
   description: "Interview assignment for Tired of Cancer (ToC)",
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
-      <header></header>
       <body>
+        <header>
+          <nav>
+            <Link href="/">Home</Link>
+            {' | '}
+            <Link href="/mylist">My List</Link>
+          </nav>
+        </header>
+
         {children}
+
+        <footer>
+        </footer>
       </body>
-      <footer></footer>
     </html>
-  )
+  );
 }
---
+++ app/mylist/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function myList() {
+    return (
+        <div>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
